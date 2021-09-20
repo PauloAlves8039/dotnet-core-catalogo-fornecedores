e2b833174389ff5ec7b8ea70abceb21518bb8e9e
PauloAlves8039 <pj38alves@gmail.com>
2021-09-20 20:53:20 -0300

    Adicionando notificações

---
+++ src/CatalogoFornecedores.Business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using CatalogoFornecedores.Business.Notificaoes;
+using System.Collections.Generic;
+
+namespace CatalogoFornecedores.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
---
+++ src/CatalogoFornecedores.Business/Notificaoes/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogoFornecedores.Business.Notificaoes
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; }
+    }
+}
