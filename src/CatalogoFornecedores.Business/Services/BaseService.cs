554bcd0ef5daa585bba5f3a7d1c4067b46264cee
PauloAlves8039 <pj38alves@gmail.com>
2021-09-20 21:26:16 -0300

    Adicionando serviços e validações

---
+++ src/CatalogoFornecedores.Business/Services/BaseService.cs
@@ -0,0 +1,41 @@
+﻿using CatalogoFornecedores.Business.Interfaces;
+using FluentValidation;
+using CatalogoFornecedores.Business.Notificaoes;
+using CatalogoFornecedores.Business.Models;
+
+namespace CatalogoFornecedores.Business.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificador _notificador;
+
+        protected BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(FluentValidation.Results.ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
---
+++ src/CatalogoFornecedores.Business/Services/ProdutoService.cs
@@ -0,0 +1,43 @@
+﻿using CatalogoFornecedores.Business.Interfaces;
+using CatalogoFornecedores.Business.Models;
+using CatalogoFornecedores.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace CatalogoFornecedores.Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoService(IProdutoRepository produtoRepository,
+                              INotificador notificador) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Adicionar(produto);
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+
+            await _produtoRepository.Atualizar(produto);
+        }
+
+        public async Task Remover(Guid id)
+        {
+            await _produtoRepository.Remover(id);
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository?.Dispose();
+        }
+    }
+}
